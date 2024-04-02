75d04270d55d91a6fa3a504c03dec042c6187988
wpthomas <31428486+wpthomas@users.noreply.github.com>
2024-04-02 08:52:10 -0400

    Changed files to pass.

---
+++ src/main/java/com/serenitydojo/exceptions/MissingWelcomeFileException.java
@@ -0,0 +1,8 @@
+package com.serenitydojo.exceptions;
+
+public class MissingWelcomeFileException extends RuntimeException{
+    public MissingWelcomeFileException(String cause) {
+        super(cause);
+    }
+
+}
